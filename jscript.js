d219b90761482e25aa9badf9dea0aef8c1f86f52
Gergito Kusse <gergito2@gmail.com>
2023-04-17 12:07:57 +0300

    Update Jscript.js file

--- jscript.js
+++ jscript.js
@@ -3,25 +3,31 @@ const uli = document.querySelector('.menu-list');
 const clbtn = document.querySelector('.close-btn');
 const nav = document.querySelector('.navigation_bar');
 const logo = document.querySelector('.navigation_bar__link');
-const lia = document.querySelectorAll('li > *');
+const lia = document.querySelector('li .li-a');
+const lia2 = document.querySelector('li .li-a2');
+const lia3 = document.querySelector('li .li-a3');
 function displaymenu() {
   nav.classList.replace('navigation_bar', 'nav-menu');
+  logo.classList.replace('navigation_bar__link', 'non-visible');
   uli.classList.replace('non-visible', 'visible');
   uli.classList.replace('menu-list', 'ul-menu');
   lia.classList.replace('li-a', 'li-a-menu');
+  lia2.classList.replace('li-a2', 'li-a-menu');
+  lia3.classList.replace('li-a3', 'li-a-menu');
   clbtn.classList.replace('non-visible', 'visible');
   humbtn.classList.add('non-visible');
-  logo.classList.replace('navigation_bar__link', 'non-visible');
 }
 humbtn.addEventListener('click', displaymenu);
 function closemenu() {
   nav.classList.replace('nav-menu', 'navigation_bar');
+  logo.classList.replace('non-visible', 'navigation_bar__link');
   uli.classList.replace('visible', 'non-visible');
   uli.classList.replace('ul-menu', 'menu-list');
-  lia.classList.remove('li-a-menu');
+  lia.classList.replace('li-a-menu', 'li-a');
+  lia2.classList.replace('li-a-menu', 'li-a2');
+  lia3.classList.replace('li-a-menu', 'li-a3');
   clbtn.classList.replace('visible', 'non-visible');
   humbtn.classList.remove('non-visible');
-  logo.classList.remove('non-visible');
 }
 clbtn.addEventListener('click', closemenu);
 uli.addEventListener('click', closemenu);
